e2451b6bfe71fc5dba53ff631988e06fcdd8a4b1
Jianfeng <554830231@qq.com>
2016-11-18 11:53:35 +0800

    增加js缓存功能

---
+++ 1/demo.js
@@ -0,0 +1,21 @@
+//缓存运算结果
+
+```
+	function add(){
+		var cache = Array.prototype.join.apply(arguments,',');
+		if(add[cache]){
+			return add[cache];
+		}else{
+			var num = 0;
+			for(var i = 0,len = arguments.length;i<len;i++){
+				if(Object.prototype.toString.apply(arguments[i]) == '[object Number]'){
+					alert("请准确输入数字参数！");
+					return 0;
+				};
+				num += arguments[i];
+			};
+			return  add[cache] = num;
+		};
+	};
+
+```
